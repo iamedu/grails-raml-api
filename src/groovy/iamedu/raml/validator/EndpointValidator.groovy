8d6d0f65e473456b367bd8ed3bec7567fa78aecd
Eduardo Díaz <iamedu@gmail.com>
2014-07-20 13:14:56 -0500

    Fixing bug with no schema

--- src/groovy/iamedu/raml/validator/EndpointValidator.groovy
+++ src/groovy/iamedu/raml/validator/EndpointValidator.groovy
@@ -167,6 +167,9 @@ class EndpointValidator {
             report,
             stringBody)
         }
+      } else {
+        def stringBody = request.JSON.toString()
+        jsonBody = JsonLoader.fromString(stringBody)
       }
     }
 
